d2f9d0afeb563bc6be545a7d552a6c277f2210a3
Mohammed <jattoe3@myumanitoba.ca>
2025-04-04 03:05:23 -0500

    Updating playbook file

--- app.py
+++ app.py
@@ -5,13 +5,12 @@ import os
 app = Flask(__name__)
 DB_PATH = os.path.join(os.path.dirname(__file__), 'data.db')
 
-INIT_SQL_PATH = os.path.join(os.path.dirname(__file__), 'init_db.sql')
-
 def init_db():
-    conn = sqlite3.connect(DB_PATH)
-    with open(INIT_SQL_PATH, 'r') as f:
-        conn.executescript(f.read())
-    conn.close()
+    if not os.path.exists(DB_PATH):
+        conn = sqlite3.connect(DB_PATH)
+        with open(os.path.join(os.path.dirname(__file__), 'init_db.sql'), 'r') as f:
+            conn.executescript(f.read())
+        conn.close()
 
 def get_messages():
     conn = sqlite3.connect(DB_PATH)
@@ -27,4 +26,5 @@ def home():
     return render_template("index.html", messages=messages)
 
 if __name__ == "__main__":
+    init_db()
     app.run(host='0.0.0.0', port=5000,debug=True)
